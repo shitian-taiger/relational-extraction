b1ca9bbf185543ed40c93995cba07101395d7a51
Shi Tian <shi.tian@taiger.com>
2019-06-19 15:39:47 +0800

    Add base implementation files

---
+++ test_implementation/lstm/base_lstm.py
@@ -0,0 +1,90 @@
+import torch
+import numpy as np
+from torch import Tensor
+from torch.nn import Parameter
+from utils import Dim_Index
+
+from typing import Dict, Optional, Tuple
+
+
+config = {
+    "input_size": 200,
+    "hidden_size": 300
+}
+
+class LSTM(torch.nn.Module):
+
+    def __init__(self, config: Dict):
+
+        super(LSTM).__init__()
+        self.input_size = config["input_size"]
+        self.hidden_size = config["hidden_size"]
+        self.weight_ih = Parameter(Tensor(self.input_size, self.hidden_size * 4))
+        self.weight_hh = Parameter(Tensor(self.hidden_size, self.hidden_size * 4))
+        self.bias = Parameter(Tensor(self.hidden_size * 4))
+        self.initialize_weights()
+
+
+    def initialize_weights(self):
+        for parameter in self.parameters():
+            if parameter.data.ndimension() >= 2:
+                torch.nn.init.xavier_uniform_(parameter.data)
+            else:
+                torch.nn.init.zeros_(parameter.data)
+
+
+    def initialize_states(self) -> Tuple[Tensor]:
+        return [torch.zeros(self.hidden_size), torch.zeros(self.hidden_size)]
+
+
+    def forward(self, x: torch.Tensor, initial_states: Optional[Tuple[torch.Tensor]]=None) -> Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        ''' Forward instance
+
+        Arguments:
+            x : Assumes input of format (batch-size, sequence-length, feature-dimensions)
+            initial_states : Optional initial cell and hidden states
+
+        Returns:
+            hidden_sequence : Output of hidden states at every time-step
+            (h_t, c_t) : Hidden and Cell states at last time-step (Imitate pytorch output format)
+        '''
+
+        batch_size, sequence_size, _ = x.size()
+
+        if initial_states is None:
+            h_t, c_t = self.initialize_states()
+        else:
+            h_t, c_t = initial_states
+
+        # Sequence computation
+        hidden_sequence = Tensor([])
+        for t in range(sequence_size):
+            x_t = x[:, t, :]
+            # Optimize singular matrix computation
+            gates = x_t @ self.weight_ih + h_t @ self.weight_hh + self.bias
+            i_t, f_t, g_t, o_t = (
+                torch.sigmoid(gates[:,                      : self.hidden_size    ]),
+                torch.sigmoid(gates[:, self.hidden_size     : self.hidden_size * 2]),
+                torch.tanh(   gates[:, self.hidden_size * 2 : self.hidden_size * 3]),
+                torch.sigmoid(gates[:, self.hidden_size * 3 :                     ]),
+            )
+            c_t = f_t * c_t + i_t * g_t
+            h_t = o_t * torch.tanh(c_t)
+            # h_t represents batch output for time_step [batch_size, output_size]
+            # unsqueeze -> [1, batch_size, output_size]
+            hidden_sequence = torch.cat([hidden_sequence, h_t.unsqueeze(0)], 0)
+
+        # [seq_len, batch_size, output_size] -> [batch_size, seq_len, output_size]
+        hidden_sequence.transpose_(Dim_Index.batch, Dim_Index.sequence_length)
+
+        return hidden_sequence, (h_t, c_t)
+
+
+optimized = LSTM(config)
+batch_size = 50
+seq_length = 20
+feature_length = 200
+
+input_arr = np.random.rand(batch_size, seq_length, feature_length)
+input_tensor = Tensor(input_arr)
+hidden_sequence, (ht, ct) = optimized.forward(input_tensor)
---
+++ test_implementation/lstm/embeddings.py
@@ -0,0 +1,86 @@
+import re
+import os
+import numpy as np
+import torch
+
+from torch.nn import Embedding
+
+class Pretrained_Embeddings:
+
+    def __init__(self, file_dir: str, file_name: str, num_dim: int):
+        """ Loads word embeddings
+
+        Args:
+            file_dir: Word embedding file directory
+            file_name: Word embedding file name
+            num_dim: Number of dimensions of embedding
+        """
+        self.file_dir = file_dir
+        self.file_name = file_name
+        self.num_dim = num_dim # Addition of UNK
+        self.word_index, self.embeddings, self.vocab_size = self.load_embeddings()
+
+
+    def load_embeddings(self):
+        """ Retrieve d_dimensional word GLOVE embeddings with n word vectors, with addition of UNK symbol,
+        Instantiate mapping of word -> index -> embedding to facilitate one-hot encoding
+
+        Returns:
+            word_index (Dict<String, List<float>>): Dictionary mapping word to index within embeddings
+            embeddings (List<List<float>>): Embeddings for each word index
+            vocab_size (int): Size of vocabulary
+        """
+        word_index = {"UNK": 0}
+        embeddings = []
+        embeddings.append(np.zeros(shape=self.num_dim, dtype='float32'))
+
+        f = open(os.path.join(self.file_dir, self.file_name))
+        for line in f:
+            values = line.split()
+            word = values[0]
+            coeffs = np.asarray(values[1:], dtype='float32')
+            word_index[word] = len(word_index)
+            embeddings.append(coeffs)
+        f.close()
+        vocab_size = len(embeddings)
+
+        return word_index, embeddings, vocab_size
+
+
+    def get_word_index(self, word: str):
+        """ Gets lowered word_index within word_index dictionary, assumes word in lower case
+
+        Returns:
+            index (int)
+        """
+        return self.word_index[word] if word in self.word_index else 0
+
+
+    def get_word_embedding(self, word: str):
+        """ Gets embedding of word, assuming lower case
+
+        Returns:
+            Embedding (List<float>)
+        """
+        word_index = self.get_word_index(word)
+        return self.embeddings[word_index]
+
+
+    def get_keras_embedding(self):
+        """ Load keras embedding layer
+
+        Returns:
+            embedding: Keras embedding layer with embeddings loaded
+        """
+        embedding_layer = Embedding(
+            num_embeddings=self.vocab_size,
+            embedding_dim=self.num_dim
+        )
+        embedding_layer.load_state_dict({ 'weight': self.embeddings })
+        embedding_layer.weight.requires_grad = False # Non-trainable
+        return embedding_layer
+
+
+def create_pretrained_embeddings(FILE_DIR, FILE_NAME):
+    NUM_DIM = int(re.search("[0-9]+d", FILE_NAME).group()[:-1])
+    return Pretrained_Embeddings(FILE_DIR, FILE_NAME, NUM_DIM)
