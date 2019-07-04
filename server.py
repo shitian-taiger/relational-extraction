cb886c5592db0515b3c18df618f4a44646b8342c
Shi Tian <shi.tian@taiger.com>
2019-07-04 09:49:22 +0800

    Set up basic front-end UI for Sentence Prediction

--- server.py
+++ server.py
@@ -1,21 +1,21 @@
+from flask_cors import CORS
 from flask import Flask, request, jsonify
 from test_implementation.trainer import Trainer
 from test_implementation.main import model_config, training_config
 
 app = Flask(__name__)
+CORS(app)
 
 trainer = Trainer(model_config, training_config)
 
 @app.route('/rel_extract/predict',methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    prediction = trainer.predict(data)
+
+    sentence = data["sentence"]
+
+    prediction = trainer.predict(sentence)
     return jsonify(prediction)
 
-@app.route('/rel_extract/add_instance',methods=['POST'])
-def add_instance():
-    data = request.get_json(force=True)
-    return jsonify("Ok")
-
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(port=8000, debug=True)
